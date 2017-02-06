fc31b78f0daa4f7f6018b47af471b0d967cb5a83
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-02-06 12:40:46 -0300

    Add method authenticate

--- boletocloud/boletocloud.py
+++ boletocloud/boletocloud.py
@@ -9,11 +9,18 @@ class Boleto(object):
 
     def __init__(self, token):
         self.__token = token
-        self.__url = 'https://sandbox.boletocloud.com/api/v1/'
+        self.__url = 'https://sandbox.boletocloud.com/api/v1/boletos/'
 
     @property
     def token(self):
-        return self.__token
+    	'''Returns the token for access API.'''
+    	return self.__token
 
-    def get_url(self, resource):
-        return '{0}{1}'.format(self.__url, resource)
+    def get_url(self, resource=''):
+    	'''Returns the url.'''
+    	return '{0}{1}'.format(self.__url, resource)
+
+    def authenticate(self, resource=''):
+    	'''Authenticates all requests.'''
+    	return requests.get(self.get_url(resource), auth=HTTPBasicAuth(self.token, 'token'))
+
--- tests/tests.py
+++ tests/tests.py
@@ -7,15 +7,19 @@ from boletocloud import Boleto
 class TestBoleto(unittest.TestCase):
 
     def setUp(self):
-        self.boleto = Boleto('12345')
-        self.url = 'https://sandbox.boletocloud.com/api/v1/'
+        self.boleto = Boleto('api-key_v0gj_lQvs0tuXgjaMHT-ebSOVsE95a3T5xbBzIIRFYE=')
+        self.url = 'https://sandbox.boletocloud.com/api/v1/boletos/'
 
     def test_token(self):
         self.assertTrue(isinstance(self.boleto, Boleto))
-        self.assertEqual(self.boleto.token, '12345')
+        self.assertEqual(self.boleto.token, 'api-key_v0gj_lQvs0tuXgjaMHT-ebSOVsE95a3T5xbBzIIRFYE=')
 
     def test_url(self):
-    	self.assertEqual(self.boleto.get_url('boleto/1'), '{0}{1}'.format(self.url, 'boleto/1'))
+    	self.assertEqual(self.boleto.get_url('vBwIcWOwGg8rteR_-n84t3PA4HCRk45IGtzRE9ulw7c='), '{0}{1}'.format(self.url, 'vBwIcWOwGg8rteR_-n84t3PA4HCRk45IGtzRE9ulw7c='))
+
+    def test_authenticate(self):
+        self.assertTrue(self.boleto.authenticate('vBwIcWOwGg8rteR_-n84t3PA4HCRk45IGtzRE9ulw7c=').status_code, 200)
+        self.assertTrue(self.boleto.authenticate('vBwIcWOwGg8rteR_-n84t3PA4HCRk45IGtzRE9ulw7c=').headers['Content-Type'], 'application/pdf; charset=UTF-8')
 
 
 if __name__ == '__main__':
